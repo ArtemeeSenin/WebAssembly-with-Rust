a77c485db52b35ba8b26964521863ca454ccb8e5
ArtemeeSenin <artemee.senin@gmail.com>
2018-11-20 01:01:02 +0300

    multiply func

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -6,8 +6,12 @@ pub fn add_one(x: u32) -> u32 {
     x + 1
 }
 #[no_mangle]
+pub fn multiply(a: u32, b: u32) -> u32 {
+  a * b
+}
+#[no_mangle]
 pub extern fn run() {
   unsafe {
-    appendNumberToBody(add_one(42));
+    appendNumberToBody(multiply(add_one(42), add_one(1000)));
   }
 }
