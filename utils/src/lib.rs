710f17f820e64ae962eb48f0d9e6613e3c4abcac
ArtemeeSenin <artemee.senin@gmail.com>
2018-11-20 01:04:43 +0300

    divide func

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -9,9 +9,14 @@ pub fn add_one(x: u32) -> u32 {
 pub fn multiply(a: u32, b: u32) -> u32 {
   a * b
 }
+pub fn divide(a: u32, b: u32) -> u32 {
+  a / b
+}
 #[no_mangle]
 pub extern fn run() {
   unsafe {
+    appendNumberToBody(multiply(add_one(42), add_one(1000)));
+    appendNumberToBody(divide(add_one(42), add_one(1000)));
     appendNumberToBody(multiply(add_one(42), add_one(1000)));
   }
 }
